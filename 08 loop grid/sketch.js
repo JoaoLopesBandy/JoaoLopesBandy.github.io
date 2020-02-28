fca99da43a42a432bdfd37b3546321095a603877
JoaoLopesBandy <60625458+JoaoLopesBandy@users.noreply.github.com>
2020-02-28 13:22:12 -0600

    color squares assignment

--- 08 loop grid/sketch.js
+++ 08 loop grid/sketch.js
@@ -1,15 +1,29 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
+let gridSpacing = 40;
+
+function mouseClicked(){
+  circleGrid();
+}
+
+function circleGrid() {
+  for (let y = gridSpacing / 2; y < height; y += gridSpacing) {
+    for (let x = gridSpacing / 2; x < width; x += gridSpacing) {
+      //let size = random(gridSpacing*0.8, gridSpacing*1.3);
+      let randRed = random(255);
+      let randGreen = random(255);
+      let randBlue = random(255);
+      fill(randRed,randGreen,randBlue);
+      rect(x, y, gridSpacing, gridSpacing);
+    }
+  }
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+  circleGrid();
 }
 
 function draw() {
-  background(220);
+  
 }
