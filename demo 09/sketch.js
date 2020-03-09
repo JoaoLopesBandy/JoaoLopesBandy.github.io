e7db21cbe5dbb279d9b4935b77da2ad5fa075c4a
JoaoLopesBandy <60625458+JoaoLopesBandy@users.noreply.github.com>
2020-03-09 13:29:31 -0600

    demo 09

--- demo 09/sketch.js
+++ demo 09/sketch.js
@@ -1,15 +1,27 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
-
+let redTime = 10;
+let redSpeed = 0.01;
+let offsetAmount = 0.02;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  ellipseMode(CENTER);
+  drawCircles();
 }
 
+function drawCircles(){
+  for (let x = 0; x < width; x += 25) {
+    for (let y = 0; y < height; y += 25) {
+      let r = map(noise(redTime), 0, 1, 0, 255);
+      fill(r,0,0);
+      ellipse(x, y, 25, 25);
+      redTime += redSpeed;
+    }
+  }
+
+}
 function draw() {
-  background(220);
+    redTime = 10 + offsetAmount;
+    background(50);
+    drawCircles();
+    offsetAmount += 0.02;
 }
