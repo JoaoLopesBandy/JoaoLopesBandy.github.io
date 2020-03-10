0d82671e487941d286d1284b16e2179147c9b3d5
JoaoLopesBandy <60625458+JoaoLopesBandy@users.noreply.github.com>
2020-03-10 13:24:59 -0600

    assignment Perlin

--- perlin noise assignment/sketch.js
+++ perlin noise assignment/sketch.js
@@ -1,29 +1,34 @@
 
 let xoff = 0;
 let yoff = 0;
-let inc = 0.01
+let inc = 0.02;
 let start = 0;
+const RECTWIDTH = 7;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CORNERS);
 
 }
 
 function draw() {
+  yoff = 0;
   background(51);
   stroke(255);
   noFill();
   beginShape();
   let xoff = start;
-  for(let x = 0; x < width; x++){
-    let y = noise(rect(xoff,yoff,xoff,yoff)) * height;
-    vertex(x,y);
+  for (let x = 0; x < width; x += RECTWIDTH) {
+    let y = map(noise(yoff), 0, 1, 0, height);
+    rect(x, height, x + RECTWIDTH, y);
 
-    xoff += 0.02;
+    yoff += 0.01;
+
   }
-  endShape();
-  
-  start += inc;
+  x += inc;
+  //endShape();
 
   
+
+
 }
