c796357dd4a86634ad19eb1bb0b46c326d7aa31d
JoaoLopesBandy <60625458+JoaoLopesBandy@users.noreply.github.com>
2020-03-13 13:10:05 -0600

    12

--- 12 generatibe art demos/sketch.js
+++ 12 generatibe art demos/sketch.js
@@ -1,15 +1,34 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
 
+let gridSpacing = 20;
+const PADDING = 20;
+
+function border() {
+  strokeWeight(15);
+  fill (100);
+  rect(width/2, height/2, width, height);
+  strokeWeight(2);
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  rectMode(CENTER);
+
 }
 
 function draw() {
-  background(220);
+  background(50);
+  border();
+  drawGrid();
+
+
+}
+
+
+function drawGrid() {
+  for (let x = gridSpacing / 2 + PADDING; x < width; x += gridSpacing) {
+    for (let y = gridSpacing / 2 + PADDING; y < height; y += gridSpacing) {
+      rect(x, y, gridSpacing, gridSpacing);
+    }
+  }
+
 }
