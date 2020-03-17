8057c4bf8d5d85b3add938b1b1834b8faadf058a
JoaoLopesBandy <60625458+JoaoLopesBandy@users.noreply.github.com>
2020-03-17 11:53:48 -0600

    wgjfalSvr

--- Interative scene-joao/sketch.js
+++ Interative scene-joao/sketch.js
@@ -1,29 +1,59 @@
-// Project Title
-// Your Name
-// Date
-//
-// Extra for Experts:
-// - describe what you did to take this project "above and beyond"
+let moveDeer = 0;
 
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-function lake(){
+function lake() {
   fill(2, 182, 237);
   ellipse(1800, 650, 700, 115)
 }
-function tree(x,y){
-  fill(1,111,111);
-  triangle(x, y, x+50, y-200, x+100, y);
+function tree(x, y) {
+  fill(1, 111, 111);
+  triangle(x + 500, y + 100, x + 550, y - 100, x + 600, y + 100);
   fill(43, 38, 22);
-  rect(538, 500, 25, 50)
+  rect(x + 538, y + 100, 25, 50)
 }
 
-function mountains(x,y){
+function deer(x, y) {
+  x = moveDeer;
+
+  fill(186, 135, 89);
+  rect(x - 50, y, 15, 80);
+  rect(x + 30, y, 15, 80);
+  rect(x - 60, y - 55, 30, 50);
+  ellipse(x, y, 130, 40);
+  triangle(x - 60, y - 30, x - 90, y - 40, x - 60, y - 55);
+  fill(214, 151, 124);
+  rect(x - 50, y - 90, 15, 40);
+  rect(x - 85, y - 90, 50, 10);
+  rect(x - 60, y - 110, 5, 20);
+  rect(x - 70, y - 110, 5, 20);
+  rect(x - 40, y - 110, 5, 20);
+  rect(x - 50, y - 110, 5, 20);
+  rect(x - 85, y - 110, 5, 20);
+
+
+}
+
+function crossHair(x, y) {
+  fill(0);
+  noFill();
+  strokeWeight(9);
+  ellipse(x, y, 500, 500);
+  line(x - 250, y, x + 250, y);
+  line(x, y - 250, x, y + 250);
+  strokeWeight(1);
+}
+function bang(x,y){
+  fill()
+  ellipse()
+}
+
+function mountains(x, y) {
   fill(123, 149, 150);
-  triangle(x, y+500, x+60, y+300, x+150, y+500)
+  triangle(x, y + 500, x + 60, y + 300, x + 150, y + 500)
   triangle(250, 500, 400, 170, 470, 500)
   triangle(120, 500, 230, 200, 270, 500)
   triangle(430, 500, 600, 300, 900, 500)
@@ -35,15 +65,24 @@ function mountains(x,y){
   triangle(1620, 500, 1700, 234, 1880, 500)
 }
 
-function ground(){
+function ground() {
   fill(7, 102, 32);
-  rect(0, 500, windowWidth,600);
+  rect(0, 500, windowWidth, 600);
 }
 
 function draw() {
   background(3, 152, 252);
-  mountains(0,0);
+  mountains(0, 0);
   ground();
   lake();
-  tree(0,500)
+  tree(0, 500)
+  deer(800, 600);
+  crossHair(mouseX, mouseY);
+  if (keyIsDown(LEFT_ARROW)) {
+    moveDeer -= 10;
+  }
+
+  if (keyIsDown(RIGHT_ARROW)) {
+    moveDeer += 10;
+  }
 }
